3cff0946fb470c7b41b41a1786cddc6cce204188
Max Aussem <aussem.maxwell@gmail.com>
2022-05-16 15:07:49 -0400

    hamburger fixed and working

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,17 @@
 import { Paper, Typography, Box, Button } from "@mui/material"
-import { styled } from "@material-ui/styles"
 import { Link } from "react-router-dom"
 import { injected } from './connectors'
 import { useWeb3React } from '@web3-react/core'
 import { useTheme } from "@mui/material/styles"
 import { useMediaQuery } from "@mui/material"
+import { useState } from "react"
+import { CgClose } from "react-icons/cg"
+import { CgMenu } from "react-icons/cg"
 
 const TopBar = () => {
 
+  const [open, setOpen] = useState(false);
+  
   const { active, account, activate, deactivate } = useWeb3React()
 
   const connectMeta = async () => {
@@ -31,20 +35,15 @@ const TopBar = () => {
     background: "linear-gradient(red, yellow);" 
   }
 
-  const MenuContainer = styled("ul")(() => ({
-    display: "flex",
-    flexDirection: "column",
-    padding: "8px",
-    marginLeft: 50,
-    width: 100
-  }));
+  const hamburger = <CgMenu 
+  size="40px" color="rgb(167, 230, 255)"
+  onClick={() => setOpen(!open)}
+/>
 
-  const Bar = styled("div")(() => ({
-    content: "''",
-    width: "30px",
-    border: "1px solid #00adb5",
-    margin: "4px"
-  }));
+  const closeButton = <CgClose 
+                        size="40px" color="rgb(167, 230, 255)"
+                        onClick={() => setOpen(!open)}
+                      />
 
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down('md'));
@@ -80,11 +79,8 @@ const TopBar = () => {
         }}>
           Finance
         </Typography>
-        <MenuContainer>
-          <Bar />
-          <Bar />
-          <Bar />
-        </MenuContainer> 
+        {open ? closeButton : hamburger }
+
       </Box>}
       {!matches && <Box sx={{
         display: "flex",
@@ -102,19 +98,6 @@ const TopBar = () => {
           className="imgT"
           alt=""
         />
-        <Typography style={{
-          fontSize: 30,
-          fontWeight: 700,
-        }}>
-          Libertas
-        </Typography>
-        <Typography className="title" style={{
-          fontSize: 30,
-          fontWeight: 700,
-          color: "rgba(0, 21, 66, 0.651)"
-        }}>
-          Finance
-        </Typography>
         </Box>
         <Link to='/'>
           <Typography className="dash1">
@@ -160,6 +143,62 @@ const TopBar = () => {
           </Button>
         }
         {active ? <div className='privatize'>{account}</div> : <></>}
+      </Box>}
+
+
+
+      {open && <Box sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "flex-end",
+        padding: 2
+      }}>
+        <Link to='/'>
+          <Typography className="dash1">
+           Dashboard
+          </Typography>
+        </Link>
+        <Link to='/calc'>
+        <Typography className="dash">
+          Calculator
+        </Typography>
+        </Link>
+        <a href="https://app.yuzu-swap.com/#/swap" target="_blank" rel="noreferrer">
+          <Typography className="dash">
+            Swap
+          </Typography>
+        </a>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column"
+        }}>
+        <Typography>
+          Launch Pad
+        </Typography>
+        <Typography variant="body3" className="dash" style={{
+          marginLeft: 2,
+          color: "rgb(167, 230, 255)"
+        }}>
+          coming soon
+        </Typography>
+        </Box>
+        <a href="https://libertas-finance.gitbook.io/libertasfinance/" target="_blank" rel="noreferrer">
+          <Typography className="dash">
+           Whitepaper
+          </Typography>
+        </a>
+        {active ? 
+          <Button  variant="contained" onClick={disconnectMeta}
+          sx={buttonStyles}> 
+            Disconnect 
+          </Button> : 
+          <Button variant="contained" onClick={connectMeta}
+          sx={buttonStyles}>
+            Connect Wallet
+          </Button>
+        }
+        {active ? <div>{account}</div> : <></>}
       </Box>}
     </Paper>
 
