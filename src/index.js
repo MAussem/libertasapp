241cdbe31a361a532306ffbfcbe36407fd2ad324
Aaron Schmid <aaron@clab.rocks>
2022-08-09 16:54:30 +0200

    autoconnect when already setup a provider before

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const { provider, webSocketProvider } = configureChains(chains, [
 const client = createClient({
   provider,
   webSocketProvider,
+  autoConnect: true,
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
