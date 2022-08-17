d09322cfa6f7755d79828100702d047f4dad6b31
Max Aussem <aussem.maxwell@gmail.com>
2022-08-17 14:29:54 -0400

    changed mobile styles of Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -64,7 +64,197 @@ const Hero = () => {
   return (
     <>
       <Grid item xs={12} className={classes.hTitle}>
-        <Paper
+      {matches && (<Paper
+          elevation={10}
+          style={{
+            borderStyle: "double",
+            borderColor: "rgb(167, 230, 255, 0.2)",
+            background: "rgba(0, 21, 66, 0.651)",
+            marginTop: 40,
+            paddingTop: 30,
+            paddingBottom: 30,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+            padding={1}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                className={classes.hTitle}
+                variant="h5"
+                component="h2"
+              >
+                Portfolio
+              </Typography>
+              <Box
+                paddingX={4}
+                paddingY={2}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                }}
+              >
+                <img src="../flame.svg" alt="" className="imgT" style={{
+                  width:20
+                }}/>
+              </Box>
+              {/* {active ? <div className='privatize2'>{account}</div> : <Typography variant="subtitle1" component="h2" style={{
+                marginBottom: 10,
+                color: "grey"
+              }}> 
+                Not Connected
+              </Typography>} */}
+              <a
+                href="https://lib-dex.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  className={classe.buttonS}
+                  variant="contained"
+                  sx={buttonStyles}
+                >
+                  Buy XLB
+                </Button>
+              </a>
+              <br />
+              {/* {active ? 
+              <Button variant="outlined" onClick={disconnectMeta} 
+              sx={buttonStyles}> 
+                Disconnect 
+              </Button> :
+              <Button variant="contained" onClick={connectMeta}
+              sx={buttonStyles}>
+                Connect Wallet
+              </Button>
+              } */}
+              <a
+                href="https://www.dextools.io/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  className={classe.buttonS}
+                  variant="contained"
+                  sx={buttonStyles}
+                >
+                  View Chart
+                </Button>
+              </a>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                className={classes.hTitle}
+                variant="h5"
+                component="h2"
+                style={{
+                  marginRight:5,
+                  color: "rgb(167, 230, 255)",
+                }}
+              >
+                Holdings
+              </Typography>
+              <Typography
+                className={classes.hTitle}
+                variant="h2"
+                component="h2"
+                style={{
+                  marginRight:5,
+                  fontWeight: 700,
+                }}
+              >
+                $0
+              </Typography>
+              <Box paddingY={2}>
+                {/* <button onClick={() => fetchBalance()}>click me</button> */}
+                <Typography
+                  className={classes.hTitle}
+                  variant="subtitle2"
+                  component="h2"
+                  style={{
+                    textAlign:"center",
+                    color: "rgb(167, 230, 255)",
+                  }}
+                >
+                  {balance}
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                className={classes.hTitle}
+                variant="h5"
+                component="h2"
+                style={{
+                  color: "rgb(167, 230, 255)",
+                }}
+              >
+                Daily Earnings
+              </Typography>
+              <Typography
+                className={classes.hTitle}
+                variant="h2"
+                component="h2"
+                style={{
+                  marginRight:5,
+                  fontWeight: 700,
+                }}
+              >
+                $0
+              </Typography>
+              <Box paddingY={2}>
+                <Typography
+                  className={classes.hTitle}
+                  variant="subtitle2"
+                  component="h2"
+                  style={{
+                    textAlign:"center",
+                    color: "rgb(167, 230, 255)",
+                  }}
+                >
+                  {dailyRoi} XLB
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              {!matches && <Rebase />}
+            </Box>
+          </Box>
+        </Paper>
+
+      )}
+      {!matches && (<Paper
           elevation={10}
           style={{
             borderStyle: "double",
@@ -245,6 +435,7 @@ const Hero = () => {
             </Box>
           </Box>
         </Paper>
+        )}
       </Grid>
     </>
   );
